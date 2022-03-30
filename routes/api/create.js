e03dcc5bf1246c20be073a4e0e8b8ced172db3dc
Dax <68510253+Dax52@users.noreply.github.com>
2022-03-30 09:50:44 +0300

    Task 4 adding api with console log

---
+++ routes/api/create.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+/* Выводим полученные данные в консоль*/
+router.post('/', function(req, res) {
+    if(!req.body) return res.sendStatus(400) 
+    else
+        console.log(req.body);
+  });
+
+    
+module.exports = router;
+
+
