73cf2945060d4cc540843d9d738b09f2e9ac20fa
Dax <68510253+Dax52@users.noreply.github.com>
2022-04-14 01:07:47 +0300

    Task6 write message to database postrgresql

--- routes/api/create.js
+++ routes/api/create.js
@@ -1,11 +1,28 @@
 var express = require('express');
 var router = express.Router();
+const pgp = require('pg-promise')();
+const db = pgp('postgres://olgetkvxnuawkl:bd20265571c5140c7267b17670cafc428103d12bedb2f61c3f3d4f33a53d6289@ec2-44-194-4-127.compute-1.amazonaws.com:5432/d94ag4hspnba8d');
 
-/* Выводим полученные данные в консоль*/
+async function insertMessage(data) {
+  try {
+    return await db.one(`
+    INSERT INTO posts (id, content)
+    VALUES (NEXTVAL('messageid'),$1) RETURNING *`,
+    [data.message]);
+  }
+  catch(e) {
+    throw e;
+  }
+}
+
 router.post('/', function(req, res) {
-    if(!req.body) return res.sendStatus(400);
+    let message = req.body;
+    if(!message) return res.sendStatus(400);
     else
-        console.log(req.body);
+
+        insertMessage(message);
+      
+        // console.log(message);
         res.sendStatus(200);
   });
 
