e9efe20b5425725802534a6284b1be46a3fddec2
Dax <68510253+Dax52@users.noreply.github.com>
2022-04-14 07:52:13 +0300

    Task6 add heroku database - fixed6 (created table on heroku base....)

--- routes/api/create.js
+++ routes/api/create.js
@@ -1,8 +1,25 @@
 var express = require('express');
 var router = express.Router();
 const pgp = require('pg-promise')();
-const db = pgp('postgres://olgetkvxnuawkl:bd20265571c5140c7267b17670cafc428103d12bedb2f61c3f3d4f33a53d6289@ec2-44-194-4-127.compute-1.amazonaws.com:5432/d94ag4hspnba8d?ssl=true');
+const connectionConf = {
+  host: 'ec2-44-194-4-127.compute-1.amazonaws.com',
+  port: 5432,
+  database: 'd94ag4hspnba8d',
+  user: 'olgetkvxnuawkl',
+  password: 'bd20265571c5140c7267b17670cafc428103d12bedb2f61c3f3d4f33a53d6289',
+  ssl: {
+        // true,
+        rejectUnauthorized : false
+        
+  }
+
+};  
+const db = pgp(connectionConf);
+// const db = pgp('postgres://olgetkvxnuawkl:bd20265571c5140c7267b17670cafc428103d12bedb2f61c3f3d4f33a53d6289@ec2-44-194-4-127.compute-1.amazonaws.com:5432/d94ag4hspnba8d?ssl=true');
 // ?ssl=true
+// tls: {
+//   rejectUnauthorized: false
+// }
 async function insertMessage(data) {
   try {
     return await db.one(`
