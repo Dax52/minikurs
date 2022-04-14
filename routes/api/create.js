ffbdda4a4c06889540525025d4e8d33f6112f745
Dax <68510253+Dax52@users.noreply.github.com>
2022-04-14 06:31:56 +0300

    Task6 add heroku database - fixed3

--- routes/api/create.js
+++ routes/api/create.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var router = express.Router();
 const pgp = require('pg-promise')();
-const db = pgp('postgres://olgetkvxnuawkl:bd20265571c5140c7267b17670cafc428103d12bedb2f61c3f3d4f33a53d6289@ec2-44-194-4-127.compute-1.amazonaws.com:5432/d94ag4hspnba8d?ssl=true');
-
+const db = pgp('postgres://olgetkvxnuawkl:bd20265571c5140c7267b17670cafc428103d12bedb2f61c3f3d4f33a53d6289@ec2-44-194-4-127.compute-1.amazonaws.com:5432/d94ag4hspnba8d');
+// ?ssl=true
 async function insertMessage(data) {
   try {
     return await db.one(`
